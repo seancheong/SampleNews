802b9a557f28a8351de1add07b9ae4faab369458
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2025-08-13 21:04:03 +0900

    Initial Commit

---
+++ SampleNews/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SampleNews
+//
+//  Created by Zhen Xiong Cheong on 2025/08/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SampleNews/SampleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleNewsApp.swift
+//  SampleNews
+//
+//  Created by Zhen Xiong Cheong on 2025/08/13.
+//
+
+import SwiftUI
+
+@main
+struct SampleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
